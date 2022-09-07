6a4fd2b011cd18ffc64c029656ce708fe582e0e0
kashi-nova <miki922010@hotmail.com>
2022-09-06 19:59:58 -0500

    plantilla poderes de enemigos y de personajes

--- Accho.java
+++ Accho.java
@@ -1,51 +1,8 @@
 public class Accho implements PoderKorby{
 	private String nombre = "accho";
 	private String frase = "lanza una llama de fuego y quita al contrincante 23 HP";
-	private String clase = "Sauropsida.";
-	private String orden = "Crocodilia.";
-	private String familia = "Alligatoridae.";
-	private String genero = "Alligator.";
-	private String sonido = "Rugido.";
-	private String apariencia = "           .-._   _ _ _ _ _ _ _ _" + "\n" +
-								".-''-.__.-'00  '-' ' ' ' ' ' ' ' '-." + "\n" +
-								"'.___ '    .   .--_'-' '-' '-' _'-' '._" + "\n" +
- 								" V: V 'vv-'   '_   '.       .'  _..' '.'." + "\n" +
-   								"   '=.____.=_.--'   :_.__.__:_   '.   : :" + "\n" +
-           						"           (((____.-'        '-.  /   : :" + "\n" +
- 								"                             (((-'\\ .' /" + "\n" +
-                           		"                           _____..'  .'" + "\n" +
-                          		"                          '-._____.-'";
-
 	private String getNombre(){
 		return nombre;
-	}
-/* 
-	private String getReino(){
-		return reino;
-	}
- */
-	private String getClase(){
-		return clase;
-	}
-
-	private String getOrden(){
-		return orden;
-	}
-
-	private String getFamilia(){
-		return familia;
-	}
-
-	private String getGenero(){
-		return genero;
-	}
-
-	private String getSonido(){
-		return sonido;
-	}
-
-	private String getApariencia(){
-		return apariencia;
 	}
 
 	@Override
@@ -56,13 +13,7 @@ public class Accho implements PoderKorby{
 	public String ataque(){
 		return 
 				"Korby" + " " +
-				frase + "\n" +
-				"Clase: " + getClase() + "\n" +
-				"Orden: " + getOrden() + "\n" +
-				"Familia: " + getFamilia() + "\n" +
-				"Genero: " + getGenero() + "\n" +
-				"Sonido: " + getSonido() + "\n";
-				
+				frase + "\n" ;				
 	}
 
 	/* public static void main(String[] args){
---
+++ Dittu.java
@@ -0,0 +1,20 @@
+public class Dittu {
+    private int HP;
+    
+    private PoderDittu Dittu;
+    public void transformacionDittu(PoderDittu nuevoPoder){
+		Dittu = nuevoPoder;
+		System.out.println ("Dittu replica la habilidad de "  /* nuevoPoder.getNombre() */);
+	}
+
+    public String mostrarAtaqueDittu(){
+        return Dittu.ataque();
+    }
+
+    public String mostrarDefensaDittu(){
+        return Dittu.defensa();
+    }
+    public PoderDittu getPoder(){
+        return Dittu;
+    }
+}
