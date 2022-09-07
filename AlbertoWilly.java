5651362e20acb29d5fe8be4810ef69454ff0cfba
PaolaResendiz <brenda.resendiz.117@ciencias.unam.mx>
2022-09-06 20:42:23 -0500

    Agregue la clase AlbertoWilly

---
+++ AlbertoWilly.java
@@ -0,0 +1,18 @@
+public class AlbertoWilly implements PoderMegaman{
+	private String nombre = "Alberto Willy";
+	private String frase = "Recupera energía oscura para potenciar a uno de sus robots. Quita al enemigo 40 HP";
+	private String getNombre(){
+		return nombre;
+	}
+
+	@Override
+	public String defensa(){
+		return "";
+	}
+	
+	@Override
+	public String ataque(){
+		return 
+				"Megaman" + " " +
+				frase + "\n" ;				
+	}
