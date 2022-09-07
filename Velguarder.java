0213a7b1ff3435ee0c0b930089faab6cc429ca73
PaolaResendiz <brenda.resendiz.117@ciencias.unam.mx>
2022-09-06 20:52:02 -0500

    Agregue la clase Verguarder

---
+++ Velguarder.java
@@ -0,0 +1,18 @@
+public class Velguarder implements PoderMegaman{
+	private String nombre = "Velguarder";
+	private String frase = "Absorbe el miedo que tenga su oponente y ataca con sus garras de hierro a este. Quita al enemigo 32 HP.";
+	private String getNombre(){
+		return nombre;
+	}
+
+	@Override
+	public String defensa(){
+		return "";
+	}
+	
+	@Override
+	public String ataque(){
+		return 
+				"Megaman" + " " +
+				frase + "\n" ;				
+	}
