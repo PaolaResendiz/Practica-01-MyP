8ba7935bd030450c0e5ee78ad43346464b8adb3f
PaolaResendiz <brenda.resendiz.117@ciencias.unam.mx>
2022-09-06 20:47:27 -0500

    Agregue la clase LaunchOctopus

---
+++ LaunchOctopus.java
@@ -0,0 +1,18 @@
+public class LaunchOctopus implements PoderMegaman{
+	private String nombre = "Launch Octopus";
+	private String frase = "Los tentáculos de Octopus absorben la energía del usuario. Infringe 30 HP al contrincante.";
+	private String getNombre(){
+		return nombre;
+	}
+
+	@Override
+	public String defensa(){
+		return "";
+	}
+	
+	@Override
+	public String ataque(){
+		return 
+				"Megaman" + " " +
+				frase + "\n" ;				
+	}
